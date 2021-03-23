8d5d0bd80ffc495fcd8b6935f49e371e18ae893e
ovansa <aminmuhammad18@gmail.com>
2021-03-23 18:31:37 +0100

    Added populate feature to getBootcamps to return courses in bootcamps, virtuals to getCourses to return the bootcamp a course belongs to, and cascade delete to delete courses in a bootcamp when a bootcamp is deleted

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -23,7 +23,10 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate({
+    path: "courses",
+    select: "title description"
+  });
 
   // Select fields
   if (req.query.select) {
@@ -121,13 +124,16 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
+
   res.status(200).json({ success: true, bootcamp: {} });
 });
 
